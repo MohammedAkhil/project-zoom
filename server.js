b7793d964080ebd5cf10d0ccc7a7b3ba41e85c73
Akhil Mohammed <mohammed.akhil@gyanmatrix.com>
2018-01-28 13:48:43 +0530

    updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-const index = require('./api/index.route');
+const index = require('./route');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/recordings',index);
